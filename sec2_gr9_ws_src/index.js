21acaeb9025587859797acac7415707a67125a64
PanithiMick <82084706+Panithi6388182@users.noreply.github.com>
2022-12-21 12:14:32 +0700

    Update index.js

--- sec2_gr9_ws_src/index.js
+++ sec2_gr9_ws_src/index.js
@@ -17,8 +17,8 @@ app.use(bodyParser.json());
 app.use("/", router);
 
 
-app.listen(8080, function(){
-    console.log("listening at Port " + 8080);
+app.listen(3030, function(){
+    console.log("listening at Port " + 3030);
 });
 
 var dbconn = mysql.createConnection({
